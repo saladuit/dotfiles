df767b7a6e2b8c88e51e986f6533a59cd6b14136
Saladin <safoh@student.codam.nl>
2023-03-28 17:08:39 +0200

    Finnally no white lines in gruvbox

--- astronvim/user/plugins/community.lua
+++ astronvim/user/plugins/community.lua
@@ -4,6 +4,8 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.feline-nvim" },
   { import = "astrocommunity.colorscheme.gruvbox" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.bash" },
