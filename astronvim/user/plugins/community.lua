891757560be7c64c24b7edb0e016acf6e4afdbd3
Saladin <safoh@student.codam.nl>
2023-05-23 19:03:01 +0200

    Removed copilot community as it sucks

--- astronvim/user/plugins/community.lua
+++ astronvim/user/plugins/community.lua
@@ -3,11 +3,8 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
-  -- { import = "astrocommunity.bars-and-lines.feline-nvim" },
   { import = "astrocommunity.colorscheme.gruvbox" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.json" },
@@ -19,5 +16,4 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.completion.copilot-lua-cmp"}
 }
