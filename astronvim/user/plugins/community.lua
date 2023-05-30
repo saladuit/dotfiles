7e67dfdfb0a5e7508378fc10e4b6f63fa7f3f266
Saladin <safoh@student.codam.nl>
2023-05-30 10:37:53 +0200

    finally fixed copilot again

--- astronvim/user/plugins/community.lua
+++ astronvim/user/plugins/community.lua
@@ -1,10 +1,11 @@
 return {
   -- Add the community repository of plugin specifications
+  "ellisonleao/gruvbox.nvim",
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
-  { import = "astrocommunity.colorscheme.gruvbox" },
+  -- { import = "astrocommunity.colorscheme.gruvbox" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.json" },
@@ -16,4 +17,20 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<Tab>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
 }
