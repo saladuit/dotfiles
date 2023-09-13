66803bf4821d953e4dd0ea5d53b13fce6dd7aac8
Saladin Afoh <safoh@student.codam.nl>
2023-09-13 16:04:30 +0200

    Save

--- astronvim/user/plugins/mason.lua
+++ astronvim/user/plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "clangd","pyright"},
+      ensure_installed = { "clangd", "pyright" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
